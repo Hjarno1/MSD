ddfc67d47396b85c19e8b10333e40586d0fc69e9
Sebjen0209 <70900449+Sebjen0209@users.noreply.github.com>
2023-10-13 09:53:24 +0200

    add SafeAreaView to App.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
