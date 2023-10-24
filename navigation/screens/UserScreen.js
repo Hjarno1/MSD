e7d7cbf40cd5aec3177b326ee8fdce6b8c269fcd
CasperKirud <ckirud@gmail.com>
2023-10-24 16:00:30 +0200

    progress

--- navigation/screens/UserScreen.js
+++ navigation/screens/UserScreen.js
@@ -1,14 +1,34 @@
 import * as React from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
-export default function UserScreen({navigation}) {
-    return (
-        <View style={{ flex: 1, alignItems: 'center'}}>
-            <Text
-                onPress={() => navigation.navigate('Home')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>
-                This is User
+export default function UserScreen({ navigation }) {
+  return (
+    <SafeAreaView style={styles.container}>
+        <View style={{alignItems: 'center'}}>
+            <Text style={{ fontSize: 26, fontWeight: 'bold'}}> Your Account {'\n'}</Text>
+            <View
+                style={{
+                backgroundColor: 'gray',
+                height: 1, // Adjust the height as needed
+                width: '100%', // This makes the line span the entire width of the screen
+                }}
+            />
+           <Text style={{ 
+                fontSize: 17, 
+                backgroundColor: 'gray', }}
+                > 
+                Anders Andersen {'\n'}
             </Text>
         </View>
-    );
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1
+    }
+
+
+  });
+  
