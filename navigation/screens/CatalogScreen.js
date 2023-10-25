2b1caf429774667059bdbfbb63c4dc476f02d901
CasperKirud <ckirud@gmail.com>
2023-10-25 15:07:08 +0200

    several changes to catalog screen

--- navigation/screens/CatalogScreen.js
+++ navigation/screens/CatalogScreen.js
@@ -5,85 +5,90 @@ import DateTimePicker from '@react-native-community/datetimepicker';
 import axios from "axios";
 import { faker } from '@faker-js/faker';
 
-
-
 export default function CatalogScreen() {
  const navigation = useNavigation();
 
-const createCar = () => {
-  return {
-    id: faker.string.uuid(),
-    name: faker.vehicle.vehicle(),
-    manufacturer: faker.vehicle.manufacturer(),
-    fuel: faker.vehicle.fuel(),
-    vin: faker.vehicle.vin(),
-    picture: faker.image.urlLoremFlickr({ category: 'sportscar' })
-  }
-}
+ const createCar = () => {
+    return {
+      id: faker.string.uuid(),
+      name: faker.vehicle.vehicle(),
+      manufacturer: faker.vehicle.manufacturer(),
+      fuel: faker.vehicle.fuel(),
+      vin: faker.vehicle.vin(),
+      picture: faker.image.urlLoremFlickr({ category: 'sportscar' })
+    }
+ }
 
-const createCars = (numCars = 15) => {
-  return new Array(numCars)
-    .fill(undefined)
-    .map(createCar);
-}
+ const createCars = (numCars = 15) => {
+    return new Array(numCars)
+      .fill(undefined)
+      .map(createCar);
+ }
 
-let fakeCars = createCars(15)
-
-
- /*const carItems = [
-   //{ id: '1', name: 'Tesla', model: 'Jog', picture: 'https://images.pexels.com/photos/1805053/pexels-photo-1805053.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' },
-   { id: '1', name: 'Tesla', model: 'Jog', picture: faker.image.image() },
-   { id: '2', name: 'Sej bil', model: 'Tank', picture: faker.image.image() },
-   { id: '3', name: 'Car 3', model: 'BMW', picture: faker.image.image() },
- ]; */
-
+ let fakeCars = createCars(15)
 
  const [date, setDate] = useState(new Date());
  const [mode, setMode] = useState('date');
  const [show, setShow] = useState(false);
- const [text, setText] = useState('Empty')
+ const [text, setText] = useState('')
 
  const onChange = (event, selectedDate) => {
-  const currentDate = selectedDate || date;
-  setShow(false); // Close the picker
-  setDate(currentDate);
+    const currentDate = selectedDate || date;
+    setShow(false);
+    setDate(currentDate);
 
-  let tempDate = new Date(currentDate);
-  let fDate = tempDate.getDate() + '/'+ (tempDate.getMonth() + 1) + '/' + tempDate.getFullYear(); // Adding 1 to the month to get the correct month
-  let fTime = 'at ' + tempDate.getHours() + ':' + tempDate.getMinutes(); 
+    let tempDate = new Date(currentDate);
+    let fDate = tempDate.getDate() + '/'+ (tempDate.getMonth() + 1) + '/' + tempDate.getFullYear();
+    let fTime = 'at ' + tempDate.getHours() + ':' + tempDate.getMinutes(); 
 
-  const selectedDateTime = fDate + ' ' + fTime + '';
-  setText(selectedDateTime); // Update the 'text' state variable with the selected date and time
+    const selectedDateTime = fDate + ' ' + fTime + '';
+    setText(selectedDateTime);
  }
 
  const showMode = (currentMode) => {
-  setShow(true);
-  setMode(currentMode);
+    setShow(true);
+    setMode(currentMode);
  }
 
  return (
    <SafeAreaView style={styles.container}>
      <View>
-     <Text style = {{textAlign: 'center', fontSize: 16}}>{text}</Text>
-      <View style = {styles.pickersOuter}>
+       <Text style={{ textAlign: 'center', fontSize: 16 }}>{text}</Text>
+       <View style={styles.pickersOuter}>
         <View style={styles.pickersInner}>
-          <Button title='DatePicker' onPress={() => showMode('date')}/>
+          <Button
+            title="DatePicker"
+            onPress={() => showMode("date")}
+            color="#1EA896" // Set the color to match the background color
+          />
         </View>
         <View style={styles.pickersInner}>
-          <Button title='TimePicker' onPress={() => showMode('time')}/>
+          <Button
+            title="TimePicker"
+            onPress={() => showMode("time")}
+            color="#1EA896" // Set the color to match the background color
+            style={{ borderRadius: 20 }} // Add the borderRadius style
+          />
         </View>
       </View>
-
-      {show && (
-        <DateTimePicker
-        testID='dateTImePicker'
-        value={date}
-        mode={mode}
-        is24Hour={true}
-        display='default'
-        onChange={onChange}
-      />)}
-    </View>
+       <View
+         style={{
+           backgroundColor: '#1EA896',
+           height: 4.5,
+           width: '100%',
+         }}
+       />
+       {show && (
+         <DateTimePicker
+           testID='dateTImePicker'
+           value={date}
+           mode={mode}
+           is24Hour={true}
+           display='default'
+           onChange={onChange}
+         />
+       )}
+     </View>
 
      <FlatList
        data={fakeCars}
@@ -92,8 +97,8 @@ let fakeCars = createCars(15)
          <View style={styles.fakeCars}>
            <Image source={{ uri: item.picture }} style={styles.foto} />
            <View style={styles.detailButtonContainer}>
-            <Text style={styles.carTitle}>{item.name}</Text>
-            <TouchableOpacity
+             <Text style={styles.carTitle}>{item.name}</Text>
+             <TouchableOpacity
                style={styles.detailButton}
                onPress={() => {
                  navigation.navigate('CarDetails', { car: item });
@@ -119,8 +124,7 @@ const styles = StyleSheet.create({
     height: 200,
     backgroundColor: '#f0f0f0',
     borderColor: '#1EA896',
-    borderWidth: 2,
-    marginBottom: 10,
+    borderBottomWidth: 5,
   },
   pickersOuter: {
     flexDirection: 'row',
